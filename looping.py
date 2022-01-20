5163bd84f694a3bc64ec6d0d140108cb2a16d90d
HenganeShubham <shubhamhengane123@gmail.com>
2022-01-20 08:14:31 +0530

    Added even_odd logic with filter function

--- looping.py
+++ looping.py
@@ -1,3 +1,7 @@
 List2 = [[2, 3, 4], [1, 4, 16, 64], [3, 6, 9, 12]]
 for i in List2:
     print(i)
+
+list3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+newlist = list(filter(lambda x: x % 2 == 0, list3))
+print(newlist)
